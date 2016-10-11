030602b65cd9ad7ff1f10d8c099a2bd2f62dc145
Sam <biryukwolf@yahoo.com>
2016-10-12 02:39:40 +0300

    empty pro

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost7/admin/');
+define('HTTP_CATALOG', 'http://localhost7/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost7/admin/');
+define('HTTPS_CATALOG', 'http://localhost7/');
+
+// DIR
+define('DIR_APPLICATION', 'W:\vhosts\localhost7/admin/');
+define('DIR_SYSTEM', 'W:\vhosts\localhost7/system/');
+define('DIR_DATABASE', 'W:\vhosts\localhost7/system/database/');
+define('DIR_LANGUAGE', 'W:\vhosts\localhost7/admin/language/');
+define('DIR_TEMPLATE', 'W:\vhosts\localhost7/admin/view/template/');
+define('DIR_CONFIG', 'W:\vhosts\localhost7/system/config/');
+define('DIR_IMAGE', 'W:\vhosts\localhost7/image/');
+define('DIR_CACHE', 'W:\vhosts\localhost7/system/cache/');
+define('DIR_DOWNLOAD', 'W:\vhosts\localhost7/download/');
+define('DIR_LOGS', 'W:\vhosts\localhost7/system/logs/');
+define('DIR_CATALOG', 'W:\vhosts\localhost7/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'localhost7');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost7/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost7/');
+
+// DIR
+define('DIR_APPLICATION', 'W:\vhosts\localhost7/catalog/');
+define('DIR_SYSTEM', 'W:\vhosts\localhost7/system/');
+define('DIR_DATABASE', 'W:\vhosts\localhost7/system/database/');
+define('DIR_LANGUAGE', 'W:\vhosts\localhost7/catalog/language/');
+define('DIR_TEMPLATE', 'W:\vhosts\localhost7/catalog/view/theme/');
+define('DIR_CONFIG', 'W:\vhosts\localhost7/system/config/');
+define('DIR_IMAGE', 'W:\vhosts\localhost7/image/');
+define('DIR_CACHE', 'W:\vhosts\localhost7/system/cache/');
+define('DIR_DOWNLOAD', 'W:\vhosts\localhost7/download/');
+define('DIR_LOGS', 'W:\vhosts\localhost7/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'localhost7');
+define('DB_PREFIX', 'oc_');
+?>
